4c760e48ab53ffbc8ac409335d9dae705c7f29b8
4onen <mattjdupree@gmail.com>
2021-08-21 17:24:34 -0700

    Fix mod name, Fix ECK ASTHook use.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
+import gc
 from modloader.modclass import Mod, loadable_mod
+from modloader import modast
 
 @loadable_mod
 class AWSWMod(Mod):
     @staticmethod
     def mod_info():
-        return ("Linkfile Example Mod", "v1.0", "4onen", False)
+        return ("Linkmod", "v1.0", "4onen", False)
 
     @staticmethod
     def mod_load():
@@ -12,4 +14,9 @@ class AWSWMod(Mod):
 
     @staticmethod
     def mod_complete():
-        pass
+        # Fixing EvilChaosKnight's bad ASTHook use.
+        # Do not, I repeat, DO NOT, hook to the main menus like he does.
+        mainmenu = modast.find_label("mainmenu")
+        bad_hooks = (node for node in gc.get_objects() if isinstance(node, modast.ASTHook) and node.next is mainmenu)
+        for hook in bad_hooks:
+            hook.unhook()
